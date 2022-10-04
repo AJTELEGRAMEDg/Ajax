be02d6c921ffd683f5a5b962d0c82231a3c2ea2c
AJTELEGRAMEDg <111381047+AJTELEGRAMEDg@users.noreply.github.com>
2022-10-04 10:38:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -117,7 +117,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🔰JOIN CHANNEL🔰, url='https://t.me/+Kmm1qgRGJANkODll') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🔰JOIN CHANNEL'🔰, url='https://t.me/+Kmm1qgRGJANkODll') ] ] ),
         )
                     
 
